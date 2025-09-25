11101035fd992ce2dae9b72a68caec59aa4a73a2
dinhcaradoc <kwambugualex@gmail.com>
2025-09-25 14:57:58 +0300

    refactor(print): migrate controller to GET /download/:otp
    - Drop legacy POST handler and EJS template
    - Delegate OTP validation and file serving to printerAuth

--- server/binafsi/controllers/apis/print.js
+++ server/binafsi/controllers/apis/print.js
@@ -3,13 +3,8 @@
 
 const express = require('express');
 const printingService = require('../../services/authentication/printerAuth');
-
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('print.ejs', { title: 'DigiBox Print' });
-});
-
-router.post('/download', printingService.printDoc);
+router.get('/download/:otp', printingService.printDoc);
 
 module.exports = router;
